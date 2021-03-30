5ce6b182fc9527e91d604a72e6b4aaf1e7278fad
Angus Salkeld <asalkeld@redhat.com>
2021-03-30 11:43:48 +1000

    Fix test usage of ironic image

--- hack/local_ironic.sh
+++ hack/local_ironic.sh
@@ -3,7 +3,8 @@
 set -xe
 
 sudo podman run -d --net host --name mariadb --entrypoint /bin/runmariadb quay.io/metal3-io/ironic:master
-sudo podman run -d --net host --name ironic -e "PROVISIONING_IP=127.0.0.1" quay.io/metal3-io/ironic:master
+sudo podman run -d --net host --name ironic-api --entrypoint /bin/runironic-api -e "PROVISIONING_IP=127.0.0.1" quay.io/metal3-io/ironic:master
+sudo podman run -d --net host --name ironic-conductor --entrypoint /bin/runironic-conductor -e "PROVISIONING_IP=127.0.0.1" quay.io/metal3-io/ironic:master
 sudo podman run -d --net host --name ironic-inspector -e "PROVISIONING_IP=127.0.0.1" quay.io/metal3-io/ironic-inspector:master
 
 for p in 3306 6385 5050; do
