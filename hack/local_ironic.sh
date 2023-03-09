297136d97cdb22c071d4ec9ab77b15a185422557
Dmitry Tantsur <dtantsur@protonmail.com>
2023-03-09 09:41:09 +0100

    Replace Travis with Github Actions

--- hack/local_ironic.sh
+++ hack/local_ironic.sh
@@ -2,11 +2,17 @@
 
 set -xe
 
-sudo podman run -d --net host --name mariadb --entrypoint /bin/runmariadb quay.io/metal3-io/ironic:master
-sudo podman run -d --net host --name ironic-api --entrypoint /bin/runironic-api -e "PROVISIONING_IP=127.0.0.1" quay.io/metal3-io/ironic:master
-sudo podman run -d --net host --name ironic-conductor --entrypoint /bin/runironic-conductor -e "PROVISIONING_IP=127.0.0.1" quay.io/metal3-io/ironic:master
-sudo podman run -d --net host --name ironic-inspector -e "PROVISIONING_IP=127.0.0.1" quay.io/metal3-io/ironic-inspector:master
+if $(which podman > /dev/null 2>&1); then
+    CONTAINER_RUNTIME=podman
+else
+    CONTAINER_RUNTIME=docker
+fi
 
-for p in 3306 6385 5050; do
+sudo $CONTAINER_RUNTIME run -d --net host --privileged --name ironic \
+    --entrypoint /bin/runironic -e "PROVISIONING_IP=127.0.0.1" quay.io/metal3-io/ironic:master
+sudo $CONTAINER_RUNTIME run -d --net host --privileged --name ironic-inspector \
+    -e "PROVISIONING_IP=127.0.0.1" quay.io/metal3-io/ironic-inspector:master
+
+for p in 6385 5050; do
   nc -z -w 60 127.0.0.1 ${p}
 done
